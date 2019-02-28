e5dfdfe29f1bc1a1ca6b0ae12b9009c597925905
Aspen James <aspenjames@tqca.org>
2019-02-28 11:20:32 -0800

    index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # This %i() syntax is another way to generate an array of symbols
   # when you pass in whitespace separated words, they are each
   # converted into symbols and put in an array. %i(this is cool) => [:this, :is, :cool]
-  
+
   resources :projects, :only => %i(index show)
   resources :users, :only => %i(index show)
-  resources :backings, :only => %i(index show)
+  resources :backings, :only => %i(index)
 
 end
